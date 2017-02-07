82b9058678834c9f9720abc13439109e776d87e2
sammeyer2017 <sam.meyer@insa-lyon.fr>
2017-02-07 10:37:59 +0100

    Added script to generate installation files, with only required files

---
+++ make_zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf threadna_install
+mkdir threadna_install
+cp -r add_structure.py calculate_energy.py DNApp.py exec.sh globvar.py INSTALL README seqmotifs.py example occupancy.py struc_list.py util.py param structures threadna_install
+chmod u+x threadna_install/INSTALL
+chmod u+x threadna_install/exec.sh
+zip -r threadna_install.zip threadna_install
